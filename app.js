11d4689188e537b1b4c1f10bedf7cc5307f5ac8a
Kevin Le <lekevin.555@gmail.com>
2026-04-21 20:49:48 -0400

    Update TMDB API key in app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_KEY = 'YOUR_TMDB_API_KEY';
+const API_KEY = 'ce8cc57d5a729929765f42f9ebcfbc1c';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
 
